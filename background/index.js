7189002f1e25ae91289b90ee88e6a61faf8d9889
Avi Herman <aviherman5@gmail.com>
2025-03-06 15:58:48 -0500

    Chrome fix

--- background/index.js
+++ background/index.js
@@ -1,3 +1,23 @@
+// Import all required scripts
+importScripts(
+  '/vendor/markdown-it.min.js',
+  '/vendor/marked.min.js',
+  '/vendor/remark.min.js',
+  '/background/md.js',
+  '/background/compilers/markdown-it.js',
+  '/background/compilers/marked.js',
+  '/background/compilers/remark.js',
+  '/background/storage.js',
+  '/background/webrequest.js',
+  '/background/detect.js',
+  '/background/inject.js',
+  '/background/messages.js',
+  '/background/mathjax.js',
+  '/background/xhr.js',
+  '/background/icon.js'
+);
+
+// Then initialize everything
 (() => {
   var storage = md.storage(md);
   var inject = md.inject({storage});
